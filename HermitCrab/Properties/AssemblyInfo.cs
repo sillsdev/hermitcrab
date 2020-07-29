dc372d4467330a99bfd274de7a12effd62384506
Eberhard Beilharz <eb1@sil.org>
2020-07-29 17:49:24 +0200

    Create nuget package

--- HermitCrab/Properties/AssemblyInfo.cs
+++ HermitCrab/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1ce57d8b-724d-49b9-b7a2-01abcb160f82")]
 
-[assembly: InternalsVisibleTo("SIL.HermitCrab.Tests")]
+[assembly: InternalsVisibleTo("SIL.HermitCrab.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010051a430c9dbe5e06a3881f8bec65a961ce96679f8b3b65e94e86aadc67306fac51fc91b968b1aeacc310caae0e52b3e458f0af44af96e229bda0de4f6a639ba43024da78c77c47e5462c30d3e569cc072fe03664db55b6f53ab8be969adccda2e6c240864b2cd0ac255db77cc710dfcbf242b088ea83a43d06bb57a1830afcbe9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
